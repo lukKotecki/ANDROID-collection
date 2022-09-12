d9fb56e179cfca995cb831a9f0e2479b7028bc59
Lukas <lukkotecki@gmail.com>
2022-09-12 20:56:56 +0200

    drugie

---
+++ Aplikacja_2022_09v2/app/src/main/java/com/example/aplikacja_2022_09v2/PrawyActivity.java
@@ -0,0 +1,13 @@
+package com.example.aplikacja_2022_09v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrawyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prawy);
+    }
+}
