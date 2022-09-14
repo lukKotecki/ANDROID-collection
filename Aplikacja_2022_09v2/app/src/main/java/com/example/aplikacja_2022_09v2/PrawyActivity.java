bc89f8926bb07ccc6c6db7e50b42854c85de58a3
Lukas <lukkotecki@gmail.com>
2022-09-14 20:59:09 +0200

    inny kurs

--- Aplikacja_2022_09v2/app/src/main/java/com/example/aplikacja_2022_09v2/PrawyActivity.java
+++ Aplikacja_2022_09v2/app/src/main/java/com/example/aplikacja_2022_09v2/PrawyActivity.java
@@ -1,13 +1,31 @@
 package com.example.aplikacja_2022_09v2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class PrawyActivity extends AppCompatActivity {
+
+
+    public static final String EXTRA_MESSAGE = "com.example.aplikacja_2022_09v2.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prawy);
     }
+
+
+    public void sendMessage(View view){
+        Intent intent= new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+
+    }
+
+
 }
