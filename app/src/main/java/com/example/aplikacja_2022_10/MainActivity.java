3ba198ebfa235dcdd37d8a1b0c6a028440bf70fc
lukas <lukkotecki@gmail.com>
2022-09-29 12:11:31 +0200

    konczenie pracy przy dejzezdzie do pracy

--- app/src/main/java/com/example/aplikacja_2022_10/MainActivity.java
+++ app/src/main/java/com/example/aplikacja_2022_10/MainActivity.java
@@ -3,25 +3,43 @@ package com.example.aplikacja_2022_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mainButton;
+    private TextView mainText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mainButton = findViewById(R.id.mainButton);
+        mainButton = findViewById(R.id.mainButton);
+        mainText = findViewById(R.id.mainText);
+
         mainButton.setOnClickListener(this);
         mainButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
 
-                Toast.makeText(MainActivity.this, "Długo kliknięto ten klawisz", Toast.LENGTH_SHORT).show();
-                return true;
+                Toast.makeText(MainActivity.this, "Długo kliknięto ten klawisz", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+        mainButton.setOnHoverListener(new View.OnHoverListener() {
+            @Override
+            public boolean onHover(View view, MotionEvent motionEvent) {
+
+                mainText.setText("BubaBuba");
+
+
+                return false;
             }
         });
 
