c09557b232b024edaca0066649347de07cb91e79
lukas <lukkotecki@gmail.com>
2023-01-13 11:51:49 +0100

    rozpoczęcie odmian saksofonów

--- Aplikacja_2022_11/app/src/main/java/com/example/aplikacja_2022_11/VarietiesActivity.java
+++ Aplikacja_2022_11/app/src/main/java/com/example/aplikacja_2022_11/VarietiesActivity.java
@@ -13,7 +13,7 @@ public class VarietiesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_varietes);
+        setContentView(R.layout.activity_varieties);
     }
 
 
