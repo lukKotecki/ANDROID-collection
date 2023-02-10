f3899d43dee230fcb929414b365c99657ceaf0b4
lukas <lukkotecki@gmail.com>
2023-02-10 11:56:38 +0100

    Dodano logowanie

--- App_2023_01_chat_app/app/src/main/java/com/example/app_2023_01_chat_app/activities/SignInActivity.java
+++ App_2023_01_chat_app/app/src/main/java/com/example/app_2023_01_chat_app/activities/SignInActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.app_2023_01_chat_app.R;
 import com.example.app_2023_01_chat_app.databinding.ActivitySignInBinding;
+import com.example.app_2023_01_chat_app.utilities.Constants;
+import com.example.app_2023_01_chat_app.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -16,10 +21,17 @@ import java.util.HashSet;
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        if(preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
@@ -28,6 +40,11 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners() {
         binding.textCreateNewAccount.setOnClickListener(view ->
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(view -> {
+            if(isValidSIgnInDetails()) {
+                signIn();
+            }
+        });
 //        binding.buttonSignIn.setOnClickListener(view -> addDataToFirestore());
 
     }
@@ -45,5 +62,62 @@ public class SignInActivity extends AppCompatActivity {
 //                });
 //
 //    }
+
+    private void signIn(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL,binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0){
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                        preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }else{
+                        loading(false);
+                        showToast("Unable to sign in");
+                    }
+                });
+    }
+
+    private void loading(Boolean isLoading){
+        if(isLoading){
+            binding.buttonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignIn.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidSIgnInDetails(){
+        if(binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter email");
+            return false;
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            showToast("Enter valid email");
+            return false;
+        }else if(binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("Enter Password");
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+
+
 
 }
